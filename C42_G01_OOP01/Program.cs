059124f91e368482b07c163d3d843148141772d0
Karim <karim@karim>
2024-07-08 20:38:16 +0300

    Finished assignment

--- C42_G01_OOP01/Program.cs
+++ C42_G01_OOP01/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System.ComponentModel;
 using System.Data;
+using static C42_G01_OOP01.Program;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+using System.Drawing;
 
 namespace C42_G01_OOP01
 {
@@ -32,6 +36,13 @@ namespace C42_G01_OOP01
             Read = 4,
             Execute = 8
         }
+
+        public enum Color : int
+        {
+            Red,
+            Green,
+            Blue
+        }
         static void Main(string[] args)
         {
             #region Q01
@@ -45,6 +56,13 @@ namespace C42_G01_OOP01
             #endregion
 
             #region Q02
+            // Create an enum called "Season" with the four seasons(Spring, Summer, Autumn, Winter)
+            // as its members.
+            // Write a C# program that takes a season name as input from the user
+            // and displays the corresponding month range for that season.
+            // Note range for seasons (spring march to may , summer june to august,
+            // autumn September to November, winter December to February)
+
             Console.WriteLine("Please eneter a season");
             Enum.TryParse<Season>(Console.ReadLine(), true, out Season UserInput);
             Console.WriteLine(UserInput.ToString());
@@ -66,14 +84,46 @@ namespace C42_G01_OOP01
             #endregion
 
             #region Q03
+            // Assign the following Permissions(Read, write, Delete, Execute) in a form of Enum.
+            // Create Variable from previous Enum to Add and Remove Permission
+            // from variable, check if specific Permission is existed inside variable
+
             Permission Admin = (Permission)6;
-            Console.WriteLine(Admin);
+            Console.WriteLine(Admin & Permission.Execute);
             Admin = Admin ^ Permission.Delete;
             Console.WriteLine(Admin);
             #endregion
 
             #region Q04
+            // Create an enum called "Colors" with the basic colors (Red, Green, Blue) as
+            // its members. Write a C# program that takes a color name as input from
+            // the user and displays a message indicating whether the input color is a
+            // primary color or not.
 
+            Console.WriteLine("Please eneter a color");
+            string UserText = Console.ReadLine();
+            bool flag = Enum.TryParse<Color>(UserText, true, out Color UserColor);
+            if (flag)
+            {
+                Console.WriteLine($"{UserColor} is a basic color.");
+            }
+            else
+            {
+                Console.WriteLine($"{UserText} is not a basic color.");
+            }
+
+            #endregion
+
+            #region Q05
+            // Create a struct called "Point" to represent a 2D point with properties "X"
+            // and "Y". Write a C# program that takes two points as input from the user
+            // and calculates the distance between them.
+            Point UserPoint = new Point();
+            Console.Write("Enter the first point: ");
+            UserPoint.X = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second point: ");
+            UserPoint.Y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(UserPoint.CalculateDistance());
             #endregion
         }
     }
