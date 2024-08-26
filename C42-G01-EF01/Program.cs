cb09c568aecb7c654e531c3ca05185ed5220c219
Karim <karim@karim>
2024-08-26 18:56:03 +0300

    Adding some notes

--- C42-G01-EF01/Program.cs
+++ C42-G01-EF01/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace C42_G01_EF01
+﻿using C42_G01_EF01.Entity;
+
+namespace C42_G01_EF01
 {
     internal class Program
     {
@@ -8,7 +10,16 @@
             // Check Classes Student - Department
 
             // For relation Many to Many Student and Course
-            // Check Classes Student - Course - StudentCourse 
+            // Check Classes Student - Course - StudentCourse
+
+            // For relation between Topic and Course one to many
+            // Check Classes Topic - Course
+
+            // For relation Many to Many Instructor and Course
+            // Check Classes Instructor - Course - CourseInstructor
+
+            // For relation between Instructor and Department one to many
+            // Check Classes Instructor - Department
         }
     }
 }
